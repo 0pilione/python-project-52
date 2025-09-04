b6c8b45c7a687c1d3d381f6abf6c4deeaa028755
Татьяна <deepredoctober@gmail.com>
2025-09-04 19:28:28 +0300

    settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,9 +23,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if str(BASE_DIR) not in sys.path:
-    sys.path.insert(0, str(BASE_DIR))
-    
+ROOT_DIR = BASE_DIR / "code"
+if ROOT_DIR.exists() and str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
 
 
 # Quick-start development settings - unsuitable for production
