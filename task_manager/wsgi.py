81df19fc31e4996fa249286680259a129b73c500
Татьяна <deepredoctober@gmail.com>
2025-09-05 09:32:07 +0300

    ...

--- task_manager/wsgi.py
+++ task_manager/wsgi.py
@@ -8,13 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
-from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
 
