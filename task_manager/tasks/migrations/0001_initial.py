c2125ba4866141672e2afbabf0d70fa1033de789
Татьяна <deepredoctober@gmail.com>
2025-09-07 10:13:12 +0300

    migrations

--- task_manager/tasks/migrations/0001_initial.py
+++ task_manager/tasks/migrations/0001_initial.py
@@ -12,10 +12,11 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Statuses',
+            name='Tasks',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=150)),
+                ('description', models.CharField(max_length=1000, verbose_name='description')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
         ),
